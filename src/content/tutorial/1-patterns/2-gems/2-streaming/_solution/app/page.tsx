9dc95d42794a40fcbcbd0e6112489bc6bc40bc97
eric-burel <eb@lbke.fr>
2024-09-03 18:30:21 +0200

    prevent image caching

--- src/content/tutorial/1-patterns/2-gems/2-streaming/_solution/app/page.tsx
+++ src/content/tutorial/1-patterns/2-gems/2-streaming/_solution/app/page.tsx
@@ -2,7 +2,8 @@ export default function FilePage() {
     return (
         <div>
             <h1>Streaming a private image</h1>
-            <img src="/api/serve-picture" />
+            {/** Will prevent any sort of caching on any browser */}
+            <img src="/api/serve-picture?solved=1" />
         </div>
     )
 }
